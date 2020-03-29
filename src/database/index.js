dc23f6f4d371012c81b201390794cd2f6d795d03
jplps <joao.pedro.pacheco@hotmail.com>
2020-03-29 16:50:58 -0300

    Defined the sequelize connection

---
+++ src/database/index.js
@@ -0,0 +1,7 @@
+// Setting up the connection
+const Sequelize = require('sequelize');
+const dbConfig = require('../config/database');
+
+const connection = new Sequelize(dbConfig);
+
+module.exports = connection;
