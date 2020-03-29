5b855e3f7396f71220e6587d660817706d1644ce
jplps <joao.pedro.pacheco@hotmail.com>
2020-03-29 16:48:38 -0300

    Added db configs module

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+// Exporting db config from elephantsql
+module.exports = {
+	dialect: 'postgres',
+	host: 'localhost',
+	username: 'ombcplas',
+	password: 'YLPmUDdUo5uy533vxtSIYRxmilociDVe',
+	database: 'tasking',
+	define: {
+		timestamps: true,
+		underscored: true,
+	},
+};
