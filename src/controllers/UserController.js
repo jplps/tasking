a8dfc8b3c685dd4124021b06f6256f02b3f5860d
jplps <joao.pedro.pacheco@hotmail.com>
2020-03-31 09:23:01 -0300

    Refactored User Controller to use trycatch

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -5,30 +5,34 @@ module.exports = {
 	async index(req, res) {
 		const { user_id } = req.params;
 
-		const logged = await User.findByPk(user_id);
+		try {
+			if (!await User.findByPk(user_id)) {
+				return res.status(400).json({ error: 'You have to be logged in to read all Users.' });
+			}
 
-		if (!logged) {
-			return res.status(400).json({ error: 'You have to be logged in to read all Users.' });
+			const users = await User.findAll();
+
+			return res.json(users);
+		} catch (err) {
+			return res.json({ err });
 		}
-
-		const users = await User.findAll();
-
-		return res.json(users);
 	},
 
 	async store(req, res) {
 		const { user_id } = req.params;
 		const { name, email } = req.body;
 
-		const logged = await User.findByPk(user_id);
+		try {
+			if (!await User.findByPk(user_id)) {
+				return res.status(400).json({ error: 'You have to be logged in to create a User.' });
+			}
 
-		if (!logged) {
-			return res.status(400).json({ error: 'You have to be logged in to create a User.' });
+			const user = await User.create({ name, email });
+
+			return res.json(user);
+		} catch (err) {
+			return res.json({ err });
 		}
-
-		const user = await User.create({ name, email });
-
-		return res.json(user);
 	},
 
 	async update(req, res) {
@@ -36,22 +40,22 @@ module.exports = {
 		const { user_id } = req.params;
 		const { id, name, email } = req.body;
 
-		// Check if we have and logged id
-		const logged = await User.findByPk(user_id);
+		try {
+			if (!await User.findByPk(user_id)) {
+				return res.status(400).json({ error: 'You have to be logged in to update a User.' });
+			}
+			// Finding user to update
+			const user = await User.findOne({
+				where: { id }
+			});
 
-		if (!logged) {
-			return res.status(400).json({ error: 'You have to be logged in to update a User.' });
+			// Update it
+			await user.update({ name, email });
+
+			return res.json(user);
+		} catch (err) {
+			return res.json({ err });
 		}
-
-		// Finding user to update
-		const user = await User.findOne({
-			where: { id }
-		});
-
-		// Update it
-		await user.update({ name, email });
-
-		return res.json(user);
 	},
 
 	async delete(req, res) {
@@ -59,20 +63,22 @@ module.exports = {
 		const { user_id } = req.params;
 		const { id } = req.body;
 
-		const logged = await User.findByPk(user_id);
+		try {
+			if (!await User.findByPk(user_id)) {
+				return res.status(400).json({ error: 'You have to be logged in to delete a User.' });
+			}
 
-		if (!logged) {
-			return res.status(400).json({ error: 'You have to be logged in to delete a User.' });
+			// Finding user
+			const user = await User.findOne({
+				where: { id }
+			});
+
+			// Delete it!
+			await user.destroy();
+
+			return res.json('User destroyed.');
+		} catch (err) {
+			return res.json({ err });
 		}
-
-		// Finding user
-		const user = await User.findOne({
-			where: { id }
-		});
-
-		// Delete it!
-		await user.destroy();
-
-		return res.json('User destroyed.');
 	},
 };
