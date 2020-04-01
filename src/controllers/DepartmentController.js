f5710a5744b2be5d1624f423427c750efeb9d4b8
jplps <joao.pedro.pacheco@hotmail.com>
2020-04-01 13:31:27 -0300

    Added department crud and tested

--- src/controllers/DepartmentController.js
+++ src/controllers/DepartmentController.js
@@ -3,30 +3,48 @@ const Department = require('../models/Department');
 module.exports = {
 	async read(req, res) {
 		try {
-			return res.send();
+			const departments = await Department.findAll();
+
+			return res.status(200).send(departments);
 		} catch (err) {
-			return res.send({ err });
+			return res.status(400).send({ err });
 		}
 	},
 
 	async create(req, res) {
+		const { name } = req.body;
+
 		try {
-			return res.send();
+			const department = await Department.create({ name });
+
+			return res.status(200).send(department);
 		} catch (err) {
-			return res.send({ err });
+			return res.status(400).send({ err });
 		}
 	},
 
 	async update(req, res) {
+		const { id, name } = req.body;
+
 		try {
-			return res.send();
+			const department = await Department.findOne({ where: { id } });
+
+			await department.update({ name });
+
+			return res.status(200).send(department);
 		} catch (err) {
-			return res.send({ err });
+			return res.status(400).send({ err });
 		}
 	},
 
 	async delete(req, res) {
+		const { id } = req.body;
+
 		try {
+			const department = await Department.findOne({ where: { id } });
+
+			await department.destroy();
+
 			return res.send('Department destroyed.');
 		} catch (err) {
 			return res.send({ err });
